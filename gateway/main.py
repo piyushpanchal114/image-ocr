cf70fbc93d1d31840668bc5639c35033a44e6cff
Piyush Panchal <piyushpanchal114@gmail.com>
2024-11-21 18:11:35 +0530

    validate jwt func

--- gateway/main.py
+++ gateway/main.py
@@ -1,5 +1,8 @@
+import jwt
 import logging
-from fastapi import FastAPI
+import os
+import pika
+from fastapi import FastAPI, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 
 
@@ -7,3 +10,21 @@ app = FastAPI()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 logging.basicConfig(level=logging.INFO)
+
+JWT_SECRET = os.environ.get("JWT_SECRET")
+AUTH_BASE_URL = os.environ.get("AUTH_BASE_URL")
+RABBITMQ_URL = os.environ.get("RABBITMQ_URL")
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_URL))
+channel = connection.channel()
+channel.queue_declare(queue="gateway_service")
+channel.queue_declare(queue="ocr_service")
+
+
+async def jwt_validation(token: str):
+    """Validate JWT token"""
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=["HS256"])
+        return payload
+    except jwt.DecodeError:
+        raise HTTPException(status_code=401, detail="Invalid token")
