01b4cc20c48ddb3c6576c7c0cbf8ca6c60655e97
Piyush Panchal <piyushpanchal114@gmail.com>
2024-11-27 13:41:37 +0530

    API for registering user in auth

--- auth/main.py
+++ auth/main.py
@@ -2,10 +2,12 @@ import fastapi as _fastapi
 import logging
 import os
 import pika
+import sqlalchemy.orm as _orm
 
 import database as _database
 import models as _models
-
+import schemas as _schemas
+import service as _services
 
 # Environment variables
 RABBITMQ_URL = os.environ.get("RABBITMQ_URL")
@@ -31,3 +33,19 @@ def get_db():
 app = _fastapi.FastAPI()
 logging.basicConfig(level=logging.INFO)
 _models.Base.metadata.create_all(_models.engine)
+
+
+@app.post("/api/users", tags=["User Auth"])
+async def create_user(
+    user: _schemas.UserCreate,
+        db: _orm.Session = _fastapi.Depends(_services.get_db)):
+    db_user = await _services.get_user_by_email(user.email, db)
+
+    if db_user:
+        logging.info("User with this email already exists")
+        raise _fastapi.HTTPException(
+            status_code=200, detail="User with this email already exists")
+
+    user = await _services.create_user(user, db)
+    return _fastapi.HTTPException(
+        status_code=201, detail="User Registered Successfully")
