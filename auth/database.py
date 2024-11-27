21c237fb2e914e8b7ab7a69a6ae247f8254b6659
Piyush Panchal <piyushpanchal114@gmail.com>
2024-11-27 15:39:00 +0530

    update

--- auth/database.py
+++ auth/database.py
@@ -1,6 +1,6 @@
 import os
 import sqlalchemy as _sql
-import sqlalchemy.ext.declarative as _declarative
+from sqlalchemy.ext.declarative import declarative_base
 
 
 postgres_host = os.environ.get("POSTGRES_HOST")
@@ -16,4 +16,4 @@ engine = _sql.create_engine(DATABASE_URL, pool_pre_ping=True)
 
 SessionLocal = _sql.orm.sessionmaker(autocommit=False,
                                      autoflush=False, bind=engine)
-Base = _declarative.declarative_base()
+Base = declarative_base()
